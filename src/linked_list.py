335a33e397da942415dd060b19841236d863b68f
Evelinabas0612 <evelinabas0612@gmail.com>
2023-05-11 14:24:48 +0300

    added hw5

--- src/linked_list.py
+++ src/linked_list.py
@@ -1,18 +1,36 @@
 class Node:
     """Класс для узла односвязного списка"""
-    pass
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
 
 
 class LinkedList:
     """Класс для односвязного списка"""
 
+    def __init__(self):
+        self.head = None
+
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        node = Node(data)
+        if not self.head:
+            self.head = node
+        else:
+            node.next_node = self.head
+            self.head = node
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+        node = Node(data)
+
+        if not self.head:
+            self.head = node
+            return
+        tail = self.head
+        while tail.next_node:
+            tail = tail.next_node
+        tail.next_node = node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -26,4 +44,4 @@ class LinkedList:
             node = node.next_node
 
         ll_string += ' None'
-        return ll_string
+        return ll_string.strip()
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,29 @@
 """Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import pytest
+
+from src.linked_list import Node, LinkedList
+
+
+@pytest.fixture
+def fixture_node():
+    return Node()
+
+
+@pytest.fixture
+def fixture_linked_list():
+    return LinkedList()
+
+
+def test_node(fixture_node):
+    assert fixture_node.data == None
+    assert fixture_node.next_node == None
+
+
+def test_linked_list(fixture_linked_list):
+    fixture_linked_list.insert_beginning({'one': 1})
+    fixture_linked_list.insert_at_end({'two': 2})
+    assert str(fixture_linked_list) == "{'one': 1} -> {'two': 2} -> None"
+    fixture_linked_list.insert_at_end({'three': 3})
+    assert str(fixture_linked_list) == "{'one': 1} -> {'two': 2} -> {'three': 3} -> None"
+    fixture_linked_list.insert_beginning({'zero': 0})
+    assert str(fixture_linked_list) == "{'zero': 0} -> {'one': 1} -> {'two': 2} -> {'three': 3} -> None"
